95681eeeb7ed2ec5d89343ec88a0746bb7d33ea3
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-12 22:08:34 -0700

    Add files via upload

---
+++ C#103ReverseSring/Program.cs
@@ -0,0 +1,13 @@
+﻿string forward, backwards = "";
+
+Console.Write("Enter a string: ");
+forward = Console.ReadLine();
+
+
+
+for (int i = forward.Length - 1; i >= 0; i--)
+{
+    backwards += forward[i];
+}
+
+Console.WriteLine($"Reversed is {backwards}");
