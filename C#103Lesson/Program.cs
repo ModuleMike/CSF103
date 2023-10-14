b9f1ad1d5f09c6404ef774f0db1955b43c3d6f05
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-13 23:25:03 -0700

    Add files via upload

---
+++ C#103Lesson/Program.cs
@@ -0,0 +1,32 @@
+﻿string key;
+
+Console.Write("Press Any Key: ");
+key = Console.ReadLine().ToUpper();
+
+//if (key == "A" || key == "E" || key == "I" || key == "O" || key == "U")
+//{
+//    Console.WriteLine("It's A Vowel!");
+//}
+//else
+//{
+//    Console.WriteLine("It's Not A Vowel!!");
+//}
+
+
+switch (key)
+{
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        Console.WriteLine("It's A Vowel!!");
+            break;
+    case "Y":
+        Console.WriteLine("Some Times A Vowel?!?");
+        break;
+    default:
+        Console.WriteLine("Not A Vowel!!");
+        break;
+
+}
