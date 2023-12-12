e1499197856142de5602569e598c9703c5b504ce
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-12-11 23:50:56 -0800

    Add files via upload

---
+++ Password Checker/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare amount and tip percentage
+decimal amount, tipPercentage;
+
+Console.WriteLine("Tip Calculator");
+Console.WriteLine("===============");
+
+// Read user inputs
+Console.Write("Enter amount: ");
+amount = decimal.Parse(Console.ReadLine());
+
+Console.Write("Enter tip: ");
+tipPercentage = decimal.Parse(Console.ReadLine());
+
+// Print final result
+Console.WriteLine($"A {tipPercentage:p2} tip on {amount:c} is {(tipPercentage * amount):c}.");
