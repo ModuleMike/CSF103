14a65356f8d62075b0f9226a24af71c8b4e39d76
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2024-01-01 23:34:31 -0800

    Add files via upload

---
+++ PointChecker/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace ThePoint
+{
+    public class Point
+    {
+        public int _X { get; private set; }
+        public int _Y { get; private set; }
+        public Point()
+        {
+            _X = 0;
+            _Y = 0;
+        }
+        public Point(int X, int Y)
+        {
+            _X = X;
+            _Y = Y;
+        }
+
+    }
+}
---
+++ PointChecker/Program.cs
@@ -0,0 +1,13 @@
+﻿using ThePoint;
+
+Point point1 = new Point(2, 3);
+Point point2 = new Point(-4, 0);
+
+Console.WriteLine($"({point1._X},{point1._Y})");
+Console.WriteLine($"({point2._X},{point2._Y})");
+Console.WriteLine(point1._X + point1._Y);
+Console.WriteLine(point2);
+point1 = new (point2._X , point2._Y);
+
+Console.WriteLine($"({point1._X},{point1._Y})");
+Console.WriteLine($"({point2._X},{point2._Y})");
